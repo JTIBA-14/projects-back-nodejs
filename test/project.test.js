d8ea13aa10cb2a56ef94d081eaae25600c9dd12b
Javier Tibaduisa <javiertibaduisa@gmail.com>
2020-08-10 08:00:47 -0500

    chore: ajuste yml circleci

--- test/project.test.js
+++ test/project.test.js
@@ -58,6 +58,45 @@ describe('Resource Model - Projects', () => {
         })
     });
 
+    describe("Resorce fecthed by HTTP", () => {
+        
+        describe("GET /", () => {
+            // Test to get all students record
+            it("should get all projects record", (done) => {
+                 chai.request(server)
+                    .get('/api/v1/projects')
+                    .end((err, res) => {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.a('object');
+                        done();
+                    });
+             });
+            // Test to get single student record
+            it("should get a single project record", (done) => {
+                 const id = 1;
+                 chai.request(server)
+                    .get(`/api/v1/projects/${id}`)
+                    .end((err, res) => {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.a('object');
+                        done();
+                    });
+             });
+             
+            // Test to get single student record
+            it("should not get a single project record", (done) => {
+                 const id = 5;
+                 chai.request(server)
+                    .get(`/api/v1/projects/${id}`)
+                    .end((err, res) => {
+                        expect(res).to.have.status(200);
+                        done();
+                    });
+             });
+        });
+
+    });
+
     afterEach( () => {
         server.close();
     })
